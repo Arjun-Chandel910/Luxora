016ea4f053ddbd95e8be65d66a29a73469b7e988
Arjun Chandel <arjunchandel910@gmail.com>
2025-03-08 00:00:19 +0530

    added styling to singleCard component

--- frontend/src/components/listings/SingleCard.jsx
+++ frontend/src/components/listings/SingleCard.jsx
@@ -11,6 +11,7 @@ const SingleCard = () => {
     price: "",
     location: "",
     country: "",
+    image: "",
   });
 
   useEffect(() => {
@@ -23,16 +24,25 @@ const SingleCard = () => {
           price: singleCard.price,
           location: singleCard.location,
           country: singleCard.country,
+          image: singleCard.image,
         });
       }
     };
     fetchData();
+    console.log("data");
   }, []);
   return (
-    <div>
-      <h1>{card.title}</h1>
+    <div className="flex flex-col items-center justify-center text-center w-full max-w-[800px] mx-auto">
+      <h1 className="text-2xl">{card.title}</h1>
+
+      <img
+        src={card.image}
+        alt={card.title}
+        className="w-[200px] h-40 sm:w-[300px] sm:h-64 object-cover rounded-2xl"
+      />
+
       <h1>{card.description}</h1>
-      <h1>{card.price}</h1>
+      <h1>&#8377; {card.price}</h1>
       <h1>{card.location}</h1>
       <h1>{card.country}</h1>
     </div>
