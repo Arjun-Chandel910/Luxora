4261e7fbc41c1beb5d4237ee036b5f81f1c250ed
Arjun Chandel <arjunchandel910@gmail.com>
2025-04-30 23:25:04 +0530

    fixed some spelling error.

--- backend/app.js
+++ backend/app.js
@@ -25,7 +25,7 @@ app.use(
 app.use(express.urlencoded({ extended: true }));
 //connect db
 mongoose
-  .connect(provess.env.MONGO_URI)
+  .connect(process.env.MONGO_URI)
   .then(console.log("db connected"))
   .catch(() => {
     console.log("Error in db");
